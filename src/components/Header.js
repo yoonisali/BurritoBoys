22d0f01b9c86b96f9293fd964a4f704542f19bbf
yoonisali <yoonismahamamoudali@gmail.com>
2023-02-27 14:15:53 -0800

    fix header styles
    co-authored-by: Chris Loveless <chriskloveless@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return(
-    <Navbar className="bg-slate-600 font-bold pl-4">
-        <NavbarBrand href="/" className="text-yellow-600"><h2>Burrito Boys</h2></NavbarBrand>
+    <Navbar className="bg-yellow-500 font-bold pl-4">
+        <NavbarBrand href="/" className="text-green-600"><h2>Burrito Boys</h2></NavbarBrand>
         <Nav>
-          <Nav.Link href="/" className="text-stone-100"><h5>Home</h5></Nav.Link>
-          <Nav.Link href="/add-spot" className="text-stone-100"><h5>Add Spot</h5></Nav.Link>
-          <Nav.Link href="/sign-in" className="text-stone-100"><h5>Sign In</h5></Nav.Link>
+          <Nav.Link href="/" className="text-green-600"><h5>Home</h5></Nav.Link>
+          <Nav.Link href="/add-spot" className="text-green-600"><h5>Add Spot</h5></Nav.Link>
+          <Nav.Link href="/sign-in" className="text-green-600"><h5>Sign In</h5></Nav.Link>
         </Nav>
       </Navbar>
   );
