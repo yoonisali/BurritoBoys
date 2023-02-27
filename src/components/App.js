4f23fc13ea6af318ee8956ec2a080e8b7cce8625
yoonisali <yoonismahamamoudali@gmail.com>
2023-02-27 11:38:03 -0800

    add initial files and boilerplate code
    co-authored-by: Chris Loveless <chriskloveless@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TopStories from "./TopStories";
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopStories />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+{/* <div>
+  Header
+  SignIn/Out
+  BurritoControl
+</div> */}
