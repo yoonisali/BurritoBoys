3a5e2a8222e2be616c6ded1f959d72d28f16fe23
daviddgamble <daviddgamble@gmail.com>
2023-03-01 13:28:15 -0800

    Add deleteFetch function to utils.js.
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -7,6 +7,10 @@ export function GetSpotById(spotList, id) {
   return null
 }
 
+//Function for creating and updating
+//url = "/api/[Spot, Rating, or Salsa]"
+//method = "POST or PUT"
+//obj = data to be sent
 export async function BodyFetch(url, method, obj) {
   return new Promise((resolve, reject) => {
     fetch(url, {
@@ -30,4 +34,26 @@ export async function BodyFetch(url, method, obj) {
         reject(err)
       })
   })
+}
+
+//Function for deleting spot, rating, or salsa
+//type = "Spot, Rating, or Salsa"
+//id = id to be deleted
+export async function deleteFetch(type, id) {
+  return new Promise((resolve, reject) => {
+    fetch(`/api/${type}/${id}`, {
+      "method": "DELETE"
+    }).then((res) => {
+      res.json()
+        .then((jres) => {
+          resolve()
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+      .catch((err) => {
+        reject(err)
+      })
+  });
 }
